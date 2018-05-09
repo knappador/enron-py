e20594c7b6f45c7a995912ad7cbcadff5d0b9a1c
Brian Knapp <knappador@gmail.com>
2018-05-09 10:02:47 +0900

    - docstrings

--- enron/exceptions.py
+++ enron/exceptions.py
@@ -4,13 +4,17 @@ class EnronError(Exception):
 
 
 class AssetTypeError(EnronError, TypeError):
+    '''Used for Asset mismatch cases such as adding USD to EUR'''
     pass
 
 
 class AmountTypeError(EnronError, TypeError):
+    '''Passed an argument which couldn't be coerced losslessly to Decimal'''
+
     def __init__(self, *args, **kwargs):
         super().__init__("Account amounts must be Decimal or losslessly coercible")
 
 
 class DefinitionError(EnronError, ValueError):
+    '''Either you didn't define something or passed an invalid symbol or name.'''
     pass
